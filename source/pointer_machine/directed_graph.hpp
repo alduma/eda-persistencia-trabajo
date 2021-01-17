d5854da77b7e83232a5a4c1302c80ca307fbe396
alduma <37373309+alduma@users.noreply.github.com>
2021-01-17 14:33:10 -0500

    Update directed_graph.hpp

--- source/pointer_machine/directed_graph.hpp
+++ source/pointer_machine/directed_graph.hpp
@@ -1,42 +1,26 @@
 // Copyright 2021 Roger Peralta Aranibar
 #ifndef SOURCE_POINTER_MACHINE_DIRECTED_GRAPH_HPP_
 #define SOURCE_POINTER_MACHINE_DIRECTED_GRAPH_HPP_
-
 #include <cstddef>
 #include <iostream>
-
 namespace ADE {
 namespace PointerMachine {
-
 template <typename Type, typename Node>
 class DirectedGraph;
-
 template <typename Type>
 class Node {
  public:
   typedef Type data_type;
-
   Node() : data_(nullptr), forward_(nullptr), out_ptrs_size_(0) {}
-
   Node(data_type const& data, std::size_t const& out_ptrs_size)
       : data_(new data_type(data)), out_ptrs_size_(out_ptrs_size) {
     forward_ = new Node<Type>*[out_ptrs_size]();
   }
-
   data_type get_data() { return *data_; }
-
   bool set_data(data_type const& data) {
     data(new data_type(data_));
     return true;
   }
-
-  /**
-   *
-   *  \brief Insert element
-   *  Inserts an element at the location id and returns the vertex pointer for
-   * the new vertex.
-   *
-   */
   Node* insert_vertex(std::size_t const& position, data_type const& data) {
     if (out_ptrs_size_ < position) {
       throw std::out_of_range("Insert position is out of edges range.");
@@ -49,14 +33,6 @@ class Node {
     forward_[position] = new_node;
     return forward_[position];
   }
-
-  /**
-   *  \brief Attachs two nodes
-   *
-   *  Adds an edge to v and throws and exception of type std::out_of_range
-   * if position is not within the range of u nodes.
-   *
-   */
   bool update_edge(std::size_t const& position, Node* v) {
     if (out_ptrs_size_ < position) {
       throw std::out_of_range("Position out of first argument node.");
@@ -64,16 +40,6 @@ class Node {
     forward_[position] = v;
     return true;
   }
-
-  /**
-   *  \brief Access specified element
-   *
-   *  Returns a reference to the element at specified location position. If id
-   * is not within the range of the container an exception of type
-   * std::out_of_range is thrown. If no node exist at the location
-   * std::logic_error is thrown.
-   *
-   */
   Node& operator[](std::size_t const& id) const {
     if (out_ptrs_size_ < id) {
       throw std::out_of_range("Index out of node edges range.");
@@ -96,24 +62,12 @@ template <typename Type, typename Node>
 class DirectedGraph {
  public:
   typedef Type data_type;
-
   DirectedGraph(data_type const& data, std::size_t const& out_ptrs_size)
       : root_ptr_(new Node(data, out_ptrs_size)),
         out_ptrs_size_(out_ptrs_size) {}
-
   virtual ~DirectedGraph() {}
-
   Node* get_root_ptr() { return root_ptr_; }
-
   Node get_root() { return *root_ptr_; }
-
-  /**
-   *  \brief Attachs two nodes
-   *
-   *  Adds edge from u to v and throws and exception of type std::out_of_range
-   * if position is not within the range of u nodes,
-   *
-   */
   bool add_edge(Node* u, Node* v, std::size_t const& position) {
     if (u->out_ptrs_size_ < position) {
       throw std::out_of_range("Position out of first argument node.");
